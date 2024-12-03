e8510c924a50f15078b4a69ff68238dbdc32e17c
Stephen O'Dwyer <67125983+angrieralien@users.noreply.github.com>
2024-12-03 11:05:06 -0500

    format sql

--- business/domain/scrumbus/stores/scrumdb/scrumdb.go
+++ business/domain/scrumbus/stores/scrumdb/scrumdb.go
@@ -205,7 +205,8 @@ func (s *Store) QueryByUserID(ctx context.Context, userID uuid.UUID) ([]scrumbus
 		scrums
 	WHERE
 		user_id = :user_id
-	ORDER BY date_created`
+	ORDER BY 
+	    date_created`
 
 	var dbHmes []scrum
 	if err := sqldb.NamedQuerySlice(ctx, s.log, s.db, q, data, &dbHmes); err != nil {
